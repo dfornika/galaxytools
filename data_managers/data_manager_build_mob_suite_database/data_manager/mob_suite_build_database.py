b4a6f040817351463731e4f22a1263d5dc00c2fc
Dan Fornika <dfornika@gmail.com>
2019-06-21 17:42:26 -0700

    Update mob-suite data manager

--- data_managers/data_manager_build_mob_suite_database/data_manager/mob_suite_build_database.py
+++ data_managers/data_manager_build_mob_suite_database/data_manager/mob_suite_build_database.py
@@ -10,27 +10,30 @@ import os
 import string
 import subprocess
 import sys
+import uuid
 
 
 DATA_TABLE_NAME = "mob_suite_databases"
 
 
-def mob_suite_build_database_mob_init(target_directory, data_table_name=DATA_TABLE_NAME):
+def mob_suite_build_database_mob_cluster(target_directory, mob_cluster_args, database_name, data_table_name=DATA_TABLE_NAME):
 
     now = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H%M%SZ")
 
-    database_value = "_".join([
-    ])
+    database_value = str(uuid.uuid4())
 
-    database_name = " ".join([
-    ])
+    database_name = database_name
 
     database_path = database_value
 
     args = [
+        '--num_threads', mob_cluster_args['num_threads'],
+        '--infile', mob_cluster_args['infile'],
+        '--outdir', database_path,
+        '--mode', mob_cluster_args['mode'],
     ]
 
-    subprocess.check_call(['mob_init'], cwd=target_directory)
+    subprocess.check_call(['mob_cluster'] + args, cwd=target_directory)
 
     data_table_entry = {
         "data_tables": {
@@ -50,8 +53,9 @@ def mob_suite_build_database_mob_init(target_directory, data_table_name=DATA_TAB
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('data_manager_json')
-    parser.add_argument( '--threads', dest='threads', default=1, help='threads' )
-    parser.add_argument( '--mode', dest='mode', default=1, help='database construction mode' )
+    parser.add_argument('--threads', dest='threads', default=1, help='threads')
+    parser.add_argument('--input', dest='input', help='Plasmid Assemblies (.fasta)')
+    parser.add_argument('--database-name', dest='database_name', help='Database Name')
     args = parser.parse_args()
 
     data_manager_input = json.loads(open(args.data_manager_json).read())
@@ -68,12 +72,18 @@ def main():
 
     data_manager_output = {}
 
-    if str(args.mode) == 'mob_init':
-        data_manager_output = mob_suite_build_database_mob_init(
-            target_directory,
-        )
-    else:
-        sys.exit("Invalid database construction mode")
+    mob_cluster_args = {
+        'num_threads': args.threads,
+        'infile': args.input,
+        'mode': 'build',
+    }
+    
+    data_manager_output = mob_suite_build_database_mob_cluster(
+        target_directory,
+        mob_cluster_args,
+        args.database_name,
+    )
+
 
     open(args.data_manager_json, 'wb').write(json.dumps(data_manager_output))
 
