9cb01222a76ffdf89fa8d7b69f32df7c2b1f860a
Dan Fornika <dfornika@gmail.com>
2019-11-01 22:31:50 -0700

    Adjust to work with concatenated abricate reports

--- tools/pick_plasmids_containing_genes/pick_plasmids_containing_genes.py
+++ tools/pick_plasmids_containing_genes/pick_plasmids_containing_genes.py
@@ -20,12 +20,6 @@ def parse_screen_file(screen_file):
             screen.append(row)
     return screen
 
-def get_fieldnames(input_file):
-    with open(input_file) as f:
-        reader = csv.DictReader(f, delimiter="\t", quotechar='"')
-        fieldnames = reader.fieldnames
-    return fieldnames
-
 def main(args):
     # create output directory
     try:
@@ -41,18 +35,17 @@ def main(args):
     contigs_with_genes_of_interest = []
     # parse all abricate reports and determine which ones contain genes of interest
     print("\t".join(["file", "gene_detected"]))
-    for abricate_report in args.abricate_reports:
-        gene_of_interest_detected = False
-        with open(abricate_report, 'r') as f:
-            abricate_report_reader = csv.DictReader(f, delimiter="\t", quotechar='"')
-            for gene in screen:
-                for abricate_report_row in abricate_report_reader:
-                    if re.search(gene['regex'], abricate_report_row['GENE']):
-                        gene_of_interest_detected = True
-                        contigs_with_genes_of_interest.append(abricate_report_row['SEQUENCE'])
-                f.seek(0)
-                next(abricate_report_reader)
-        print("\t".join([abricate_report, str(gene_of_interest_detected)]))
+
+    with open(args.concatenated_abricate_reports, 'r') as f:
+        abricate_report_reader = csv.DictReader(f, delimiter="\t", quotechar='"')
+        for gene in screen:
+            for abricate_report_row in abricate_report_reader:
+                if abricate_report_row['#FILE'] == '#FILE':
+                    continue
+                if re.search(gene['regex'], abricate_report_row['GENE']):
+                    contigs_with_genes_of_interest.append(abricate_report_row['SEQUENCE'])
+            f.seek(0)
+            next(abricate_report_reader)
 
     # copy the corresponding plasmid fasta files into outdir        
     for contig in contigs_with_genes_of_interest:
@@ -69,7 +62,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--plasmids", nargs='+', help="plasmid assemblies (fasta)")
-    parser.add_argument("--abricate_reports", nargs='+', help="abricate reports (tsv)")
+    parser.add_argument("--concatenated_abricate_reports", help="abricate reports (tsv)")
     parser.add_argument("--abricate_report_screening_file", help="")
     parser.add_argument("--outdir", dest="outdir", default=".", help="Output directory")
     args = parser.parse_args()
